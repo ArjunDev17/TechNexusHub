58b0d45b70e4c4cb0d73aec36af05f845bb4ae5d
ArjunDev17 <arjun.devb25@gmail.com>
2024-06-23 03:19:19 +0530

    feat: Add HomeController and initial project setup for TechNexusHub
    - Added new HomeController.java to manage home page requests.
    - Initial project setup including gitignore, Maven wrapper files, pom.xml, application class, and resource directories.
    
    Changes:
    - New file: TechNexusHub/src/main/java/com/codeneeti/technexushub/controllers/HomeController.java
    - Modified: TechNexusHub/src/main/java/com/codeneeti/technexushub/controllers/HomeController.java
    - Untracked files: Initial project setup files and directories.

---
+++ TechNexusHub/src/main/java/com/codeneeti/technexushub/TechNexusHubApplication.java
@@ -0,0 +1,22 @@
+package com.codeneeti.technexushub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TechNexusHubApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TechNexusHubApplication.class, args);
+	}
+	@GetMapping("/hi")
+	public String welcomeMsg(){
+		System.out.println("coming inside");
+		return "Welcome back";
+	}
+
+}
