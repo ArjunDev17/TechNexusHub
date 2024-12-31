69f3f20f12d4329acde789bb23a261cfbd13f4f2
ArjunDev17 <arjun.devb25@gmail.com>
2024-12-31 15:10:48 +0530

    create user profile payload updated

--- TechNexusHub/src/main/java/com/codeneeti/technexushub/dtos/UserProfileDto.java
+++ TechNexusHub/src/main/java/com/codeneeti/technexushub/dtos/UserProfileDto.java
@@ -1,5 +1,10 @@
 package com.codeneeti.technexushub.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserProfileDto {
 
     private String firstName;
@@ -8,53 +13,14 @@ public class UserProfileDto {
     private String phone;
     private UserAddressDto address;
     private String profilePicture;
-
-    // Getters and Setters
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public UserAddressDto getAddress() {
-        return address;
-    }
-
-    public void setAddress(UserAddressDto address) {
-        this.address = address;
-    }
-
-    public String getProfilePicture() {
-        return profilePicture;
-    }
-
-    public void setProfilePicture(String profilePicture) {
-        this.profilePicture = profilePicture;
-    }
+    private String dob; // Date of Birth
+    private String gender;
+    private String collegeName;
+    private String course;
+    private String yearOfStudy;
+    private String cgpa;
+    private String interest; // Fixed typo from "intrest" to "interest"
+    private String skill;
+    private String linkdn;  // LinkedIn link
+    private String github;  // GitHub link
 }
--- TechNexusHub/src/main/java/com/codeneeti/technexushub/entities/UserProfile.java
+++ TechNexusHub/src/main/java/com/codeneeti/technexushub/entities/UserProfile.java
@@ -3,10 +3,10 @@ package com.codeneeti.technexushub.entities;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
@@ -15,10 +15,10 @@ import lombok.Setter;
 @Entity
 public class UserProfile {
 
-    // Getters and Setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Email
     private String email;
 
@@ -28,37 +28,19 @@ public class UserProfile {
     private String firstName;
     private String lastName;
 
-
     @Embedded
     private UserAddress address;
 
+    private String dob; // Date of Birth
+    private String gender;
     private String profilePicture;
+    private String collegeName;
+    private String course;
+    private String yearOfStudy;
+    private String cgpa;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setAddress(UserAddress address) {
-        this.address = address;
-    }
-
-    public void setProfilePicture(String profilePicture) {
-        this.profilePicture = profilePicture;
-    }
+    private String interest; // Fixed typo: "intrest" -> "interest"
+    private String skill;
+    private String linkdn;  // LinkedIn link
+    private String github;  // GitHub link
 }
